470cdfd499212d0553d6fe40c97cc3aad03a3715
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-10-30 11:00:58 +0100

    finished matplotlib scatter chart

--- rw_visual.py
+++ rw_visual.py
@@ -5,15 +5,16 @@ from random_walk import RandomWalk
 #Keep making new random walks, as long as the program is active:
 while True:
     #Make a random walk:
-    rw = RandomWalk()
+    rw = RandomWalk(50_000)
     rw.fill_walk()
 
     #Plot the points in the walk:
     plt.style.use('classic')
+    #fig, ax = plt.subplots(figsize=(10, 6), dpi=128)
     fig, ax = plt.subplots()
     point_numbers = range(rw.num_points)
     #Add colour range to the points:
-    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=15)
+    ax.scatter(rw.x_values, rw.y_values, c=point_numbers, cmap=plt.cm.Blues, edgecolors='none', s=1)
     # ax.scatter(rw.x_values, rw.y_values, s = 15)
 
     #Emphasise the first and last points:
