913e9d71ca053e1027486d8a1c354a3f6acb9433
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-10-30 10:06:11 +0100

    color gradient and saving file

--- scatter_squares.py
+++ scatter_squares.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
 
-x_values = [1, 2, 3, 4, 5]
-y_values = [1, 4, 9, 16, 25]
+x_values = range(1, 1001)
+y_values = [x**2 for x in x_values]
 
 plt.style.use('seaborn-v0_8-deep')
 fig, ax = plt.subplots()
 
-ax.scatter(x_values, y_values, s=100)
+ax.scatter(x_values, y_values, c=y_values, cmap=plt.cm.Blues, s=10)
 
 #Set the chat title and label axes:
 ax.set_title("Square Numbers", fontsize=24)
@@ -16,4 +16,11 @@ ax.set_ylabel("Square of value", fontsize=14)
 
 #Set size of tick labels:
 ax.tick_params(axis='both', which='major', labelsize=14)
+
+#Set the range for each axis:
+ax.axis([0, 1100, 0, 1100000])
+
 plt.show()
+
+#save the file:
+#plt.savefig('squares_plot.png', bbox_inches='tight')
