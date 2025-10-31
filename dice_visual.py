608b1ee7106b8486498115427ae3002100de8e4d
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-10-31 10:26:31 +0100

    data visualisation complete

--- dice_visual.py
+++ dice_visual.py
@@ -5,11 +5,11 @@ from die import Die
 
 #Create TWO D6 dice:
 die_1 = Die()
-die_2 = Die()
+die_2 = Die(10)
 
 #Make some rolls, and store results in a list:
 results = []
-for roll_num in range(1000):
+for roll_num in range(50_000):
     result = die_1.roll() + die_2.roll()
     results.append(result)
 
@@ -26,7 +26,7 @@ data = [Bar(x=x_values, y=frequencies)]
 
 x_axis_config = {'title' : 'Result', 'dtick': 1}
 y_axis_config = {'title' : 'Frequency of Result'}
-my_layout = Layout(title='Results of rolling two D6 1000 times', xaxis=x_axis_config, yaxis=y_axis_config)
-offline.plot({'data' : data, 'layout': my_layout}, filename='d6_d6.html')
+my_layout = Layout(title='Results of rolling a D6 and a D10 50,000 times', xaxis=x_axis_config, yaxis=y_axis_config)
+offline.plot({'data' : data, 'layout': my_layout}, filename='d6_d10.html')
                             
 # print(frequencies)
