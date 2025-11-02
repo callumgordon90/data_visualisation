110dae5f47c0a2777d0e3b2299cfe3b91f356c1a
CallumGordon90 <callumjosephiaingordon@gmail.com>
2025-11-02 11:52:02 +0100

    finished practice with plotly and csv data

--- death_valley_highs_lows.py
+++ death_valley_highs_lows.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib.pyplot as plt
 from datetime import datetime
 
 filename = 'data/death_valley_2018_simple.csv'
@@ -13,12 +14,15 @@ with open(filename) as f:
     dates, highs, lows = [], [], [] 
     for row in reader:
         current_date = datetime.strptime(row[2], '%Y-%m-%d')
-
-        high = int(row[4])
-        low = int(row[5])
-        dates.append(current_date)
-        highs.append(high)
-        lows.append(low)
+        try:
+            high = int(row[4])
+            low = int(row[5])
+        except ValueError:
+            print(f"Missing data for {current_date}")
+        else:
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
 
 #plot the high and low temperatures:
 plt.style.use('classic')
